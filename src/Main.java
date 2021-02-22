101d041c6d7a9c080e5d941b6055bbb36ef5282e
OryxGit <60434657+OryxGit@users.noreply.github.com>
2021-02-22 18:45:02 +0100

    hello world

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     //this is just a test class :D
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+        System.out.println("!");
     }
 }
