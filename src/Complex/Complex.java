16d5b98d4355188e6bc790b0f0c03de1f8f2229a
Daniel Ruhland <daniel.ruhland@tum.de>
2021-02-23 12:52:24 +0100

    Powers Added

--- src/Complex/Complex.java
+++ src/Complex/Complex.java
@@ -92,35 +92,74 @@ public class Complex {
     //Division of Complex Numbers
     public static Complex div(Complex z, Complex w) {
         if (w.abs == 0)
-            throw new ArithmeticException("Division by 0");
+            throw new ArithmeticException("Division by 0!");
         return Polar(z.arg - w.arg, z.abs / w.abs);
     }
 
     //Division of Complex and Real Numbers
     public static Complex div(Complex z, double w) {
         if (w == 0)
-            throw new ArithmeticException("Division by 0");
+            throw new ArithmeticException("Division by 0!");
         return Polar(z.arg, z.abs / w);
     }
 
     public static Complex div(double z, Complex w) {
         if (w.abs == 0)
-            throw new ArithmeticException("Division by 0");
+            throw new ArithmeticException("Division by 0!");
         return Polar(-w.arg, z / w.abs);
     }
 
     //Division of Real Numbers
     public static Complex div(double z, double w) {
         if (w == 0)
-            throw new ArithmeticException("Division by 0");
+            throw new ArithmeticException("Division by 0!");
         return Polar(0, z / w);
     }
 
-    //Power using Complex numbers (including complex Exponents)
     //Format: z^w
+    //Power of Complex numbers
     public static Complex pow(Complex z, Complex w) {
-        //TODO
-        return null;
+        if (z.abs == 0 && w.abs == 0)
+            throw new ArithmeticException("0^0 is undefined!");
+        else if (z.abs == 0)
+            return new Complex(0, 0);
+        else if (w.abs == 0)
+            return new Complex(1, 0);
+        else
+            return Polar(z.arg * w.Re + Math.log(z.abs) * w.Im, Math.pow(z.abs, w.Re) * Math.pow(Math.E, -z.arg * w.Im));
+    }
+
+    //Power of Complex and Real Numbers
+    public static Complex pow(Complex z, double w) {
+        if (z.abs == 0 && w == 0)
+            throw new ArithmeticException("0^0 is undefined!");
+        else if (z.abs == 0)
+            return new Complex(0, 0);
+        else if (w == 0)
+            return new Complex(1, 0);
+        else
+            return Polar(z.arg * w, Math.pow(z.abs, w));
+    }
+
+    public static Complex pow(double z, Complex w) {
+        if (z == 0 && w.abs == 0)
+            throw new ArithmeticException("0^0 is undefined!");
+        else if (z == 0)
+            return new Complex(0, 0);
+        else if (w.abs == 0)
+            return new Complex(1, 0);
+        else if(z > 0)
+            return Polar(Math.log(z)*w.Im, Math.pow(z, w.Re));
+        else
+            return Polar(Math.log(-z)*w.Im,Math.pow(z, w.Re)*Math.pow(Math.E, -Math.PI*w.Im));
+    }
+
+    //Power of Real Numbers
+    public static Complex pow(double z, double w) {
+        if (z == 0 && w == 0)
+            throw new ArithmeticException("0^0 is undefined!");
+        else
+            return new Complex(Math.pow(z, w), 0);
     }
 
     //Complex-conjugate
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import Complex.Complex;
 public class Main {
     //this is just a test class :D
     public static void main(String[] args) {
-        Complex z = new Complex(1, 0);
-        Complex w = new Complex(0, 0);
-        Complex res = Complex.add(z, 1);
-        System.out.println("Re: " + res.getRe() + " Im: " + res.getIm());
+        double z = Math.E;
+        Complex w = new Complex(0, Math.PI);
+        Complex x = Complex.pow(z, w);
+        System.out.println("Re: " + x.getRe() + " Im: " + x.getIm());
     }
 }
