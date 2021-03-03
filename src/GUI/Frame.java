64b6ee7f5251f4a6bba5b883e817e52e2b683486
OryxGit <60434657+OryxGit@users.noreply.github.com>
2021-03-03 12:40:54 +0100

    UI

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -7,20 +7,18 @@ import java.util.ArrayList;
 public class Frame extends JFrame {
     JPanel panel = new JPanel();
     int width, height;
+    ArrayList<Point> pointList = new ArrayList<Point>();
 
-    public Frame(int width, int height) {
+    public Frame(int width, int height, ArrayList<Point> points) {
         this.width = width;
         this.height = height;
+        this.pointList = points;
         this.setSize(width, height);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel.setBackground(Color.BLACK);
         this.add(panel);
     }
-
-    //temp
-    ArrayList<Point> pointList = new ArrayList<Point>();
-
 
     public void addPoint(int x, int y, Color color) {
         pointList.add(new Point(x, y, color));
@@ -54,18 +52,10 @@ public class Frame extends JFrame {
         }
     }
 
+
     public void paintMandel(int x, int y, Color color) {
 
     }
 
-    private class Point {
-        private int x, y;
-        private Color color;
 
-        Point(int x, int y, Color color) {
-            this.x = x;
-            this.y = y;
-            this.color = color;
-        }
-    }
 }
--- src/MandlebrotSet/MandlebrotSet.java
+++ src/MandlebrotSet/MandlebrotSet.java
@@ -1,14 +1,37 @@
 package MandlebrotSet;
 
-import GUI.Frame;
-
 import java.awt.*;
+import java.util.ArrayList;
+import Complex.Complex;
+import GUI.Frame;
+import GUI.Point;
+
 
 public class MandlebrotSet {
+    private static final double UNIT = 100;
+    private static ArrayList<Point> points = new ArrayList<>();
+
     public static void main(String[] args) {
-        Frame frame = new Frame(500, 500);
-        for (int i = 0; i < 100; i++) {
-            frame.addPoint(i, i*i, Color.CYAN);
+        for (int re = -200; re <= 200; re++) {
+            for (int im = -200; im <= 200; im++) {
+                int conv = converges((new Complex(re/UNIT, im/UNIT)));
+                points.add(new Point(re, im, new Color(conv)));
+                System.out.println("Re: " + re/UNIT + "Im: " + im/UNIT + "Conv: " + conv);
+            }
         }
+        Frame frame = new Frame(500, 500, points);
+    }
+
+    public static int converges(Complex c) {
+        Complex zn = c;
+        int steps = 256;
+        for (int i = 0; i < 256; i++) {
+            zn = Complex.add(Complex.mul(zn, zn), c);
+            if (zn.getAbs() >= 2) {
+                steps = i;
+                break;
+            }
+        }
+        return steps;
     }
 }
