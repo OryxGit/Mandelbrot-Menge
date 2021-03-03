b1f8a9d657973571d8a497aff89c08830da5208a
OryxGit <60434657+OryxGit@users.noreply.github.com>
2021-03-03 11:50:50 +0100

    UI

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -2,9 +2,11 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Frame extends JFrame {
     JPanel panel = new JPanel();
+    int width, height;
 
     public Frame() {
         this.setSize(500, 500);
@@ -14,9 +16,28 @@ public class Frame extends JFrame {
         this.add(panel);
     }
 
+    //temp
+    int x, y;
+    Color color;
+    ArrayList<Point> pointList = new ArrayList<Point>();
+
+
+    public void addPoint(int x, int y, Color color) {
+        pointList.add(new Point(x, y, color));
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.drawLine(0, 0, 100, 100);
+        int startX = width / 2;
+        int startY = height / 2;
+        g.translate(startX, startY);
+        g.setColor(Color.WHITE);
+        g.drawLine(-200, 0, 200, 0);
+        g.drawLine(0, 200, 0, -200);
+        for (Point p : pointList){
+            g.setColor(p.color);
+            g.drawOval(p.x,-p.y,1,1);
+        }
     }
 }
--- src/MandlebrotSet/MandlebrotSet.java
+++ src/MandlebrotSet/MandlebrotSet.java
@@ -2,6 +2,8 @@ package MandlebrotSet;
 
 import GUI.Frame;
 
+import java.awt.*;
+
 public class MandlebrotSet {
     public static void main(String[] args) {
         Frame frame = new Frame();
