9367dcabb6051920aa88d1e54f8b5de85a258874
OryxGit <60434657+OryxGit@users.noreply.github.com>
2021-03-03 11:11:35 +0100

    :)

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -1,7 +1,21 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
+    JPanel panel = new JPanel();
 
+    public Frame() {
+        this.setSize(500, 500);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(panel);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawLine(0, 0, 100, 100);
+    }
 }
