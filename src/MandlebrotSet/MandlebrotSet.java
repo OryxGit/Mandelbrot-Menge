0e0ddf6113fac2d95c7d8840d1645c8754c04b6b
OryxGit <60434657+OryxGit@users.noreply.github.com>
2021-03-03 13:50:12 +0100

    UI

--- src/MandlebrotSet/MandlebrotSet.java
+++ src/MandlebrotSet/MandlebrotSet.java
@@ -2,6 +2,8 @@ package MandlebrotSet;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
+
 import Complex.Complex;
 import GUI.Frame;
 import GUI.Point;
@@ -10,12 +12,13 @@ import GUI.Point;
 public class MandlebrotSet {
     private static final double UNIT = 100;
     private static ArrayList<Point> points = new ArrayList<>();
+    private static int val = 256;
 
     public static void main(String[] args) {
         for (int re = -200; re <= 200; re++) {
             for (int im = -200; im <= 200; im++) {
                 int conv = converges((new Complex(re / UNIT, im / UNIT)));
-                points.add(new Point(re, im, new Color(conv, conv, conv)));
+                points.add(new Point(re, im, new Color((val - conv) % 255, (val - conv) % 255, (val - conv) % 255)));
             }
         }
         Frame frame = new Frame(500, 500, points);
@@ -23,8 +26,8 @@ public class MandlebrotSet {
 
     public static int converges(Complex c) {
         Complex zn = c;
-        int steps = 255;
-        for (int i = 0; i < 255; i++) {
+        int steps = val;
+        for (int i = 1; i < val; i++) {
             zn = Complex.add(Complex.mul(zn, zn), c);
             if (zn.getAbs() >= 2) {
                 steps = i;
